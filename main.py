2e8a9b8b0b58e6f31c154ea88af854b70795d726
Kristian Mischke <kristianmischke@gmail.com>
2022-10-08 14:47:10 -0400

    receipt processing api endpoints

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from typing import Union
 
+import fastapi
 from fastapi import FastAPI
 import requests
 
 from doctr.io import DocumentFile
 from doctr.models import ocr_predictor
+from starlette.responses import PlainTextResponse, Response
+
+from receipt_processing import convert_receipt_image_to_df, convert_receipt_image_to_text
 
 model = ocr_predictor(pretrained=True)
 app = FastAPI()
+
+
+class DFResponse(Response):
+    media_type = "application/json"
+
+    def render(self, content: any) -> bytes:
+        return content.encode("utf-8")
 
 
 @app.get("/")
@@ -17,12 +28,28 @@ def read_root():
 
 @app.get("/ocr")
 def do_ocr(path: str):
-    # PDF
     response = requests.get(path)
     doc = DocumentFile.from_images(response.content)
     # Analyze
     result = model(doc)
     return result
+
+
+@app.get("/receipt_to_text",
+         response_class=PlainTextResponse
+         )
+def receipt_to_text(path: str):
+    response = requests.get(path)
+    return convert_receipt_image_to_text(response.content, model, True)
+
+
+@app.get("/receipt_to_items",
+         response_class=DFResponse
+         )
+def receipt_to_items(path: str):
+    response = requests.get(path)
+    df = convert_receipt_image_to_df(response.content, model, True)
+    return df.to_json()
 
 
 @app.get("/items/{item_id}")
---
+++ receipt_processing.py
@@ -0,0 +1,150 @@
+import math
+import re
+
+import pandas as pd
+
+from doctr.io import DocumentFile, Page
+from doctr.models.predictor import OCRPredictor
+
+
+def get_points(geometry):
+    return geometry[0][0],\
+           geometry[0][1],\
+           geometry[1][0],\
+           geometry[1][1]
+
+
+def convert_doc_page_to_text_grid(page: Page, debug: bool) -> list[str]:
+
+    # get page geometry as bounding box of all blocks in page
+    page_geometry = ((math.inf, math.inf), (-math.inf, -math.inf))
+    for block in page.blocks:
+        page_geometry = (
+            (
+                min(page_geometry[0][0], block.geometry[0][0]),
+                min(page_geometry[0][1], block.geometry[0][1]),
+            ),
+            (
+                max(page_geometry[1][0], block.geometry[1][0]),
+                max(page_geometry[1][1], block.geometry[1][1]),
+            )
+        )
+    if debug:
+        print(page_geometry)
+    pg_x, pg_y, pg_x2, pg_y2 = get_points(page_geometry)
+
+    all_words = []
+    avg_character_widths = []
+    for block in page.blocks:
+        for line in block.lines:
+            for word in line.words:
+                x, y, x2, y2 = get_points(word.geometry)
+                avg_character_widths.append((x2 - x) / len(word.value))
+                all_words.append(word)
+
+    avg_character_widths.sort()
+    char_width = avg_character_widths[0]  # char width defined by minimum avg char width
+    if debug:
+        print(avg_character_widths)
+        print(char_width)
+
+    # get number of columns
+    cols = math.ceil((pg_x2 - pg_x) / char_width)
+
+    # sort by x then by y
+    sorted_words = sorted(all_words, key=lambda word: word.geometry[0][0])
+    sorted_words = sorted(sorted_words, key=lambda word: word.geometry[0][1])
+
+    text = []
+    ln_start_y = pg_y
+    ln_end_y = pg_y
+    for word in sorted_words:
+        x, y, x2, y2 = get_points(word.geometry)
+
+        new_row = False
+        if ln_start_y == ln_end_y:
+            # first line
+            if debug:
+                print('\n', "first line", len(text), ln_start_y, ln_end_y, word, word.geometry, '\n')
+            new_row = True
+
+        if y > ln_end_y:
+            # below last row
+            if debug:
+                print('\n', "below last row", len(text), ln_start_y, ln_end_y, word, word.geometry, '\n')
+            new_row = True
+
+        if ln_end_y > y > ln_start_y:
+            # starts in between the line
+            percent_close = (y - ln_start_y) / (ln_end_y - ln_start_y)
+            percent_far = (y2 - ln_start_y) / (ln_end_y - ln_start_y)
+            if percent_close > 0.5:
+                if debug:
+                    print('\n', percent_close, "percent_close", len(text), ln_start_y, ln_end_y, word, word.geometry, '\n')
+                new_row = True
+            elif percent_far > 1.5:
+                if debug:
+                    print('\n', percent_far, "percent_far", len(text), ln_start_y, ln_end_y, word, word.geometry, '\n')
+                new_row = True
+
+        start_col = math.floor((x - pg_x) / char_width)
+
+        if len(text) > 0 and text[-1][start_col:start_col+len(word.value)].strip() != '':
+            if debug:
+                print('\n', "conflict_new_row", len(text), ln_start_y, ln_end_y, word, word.geometry, '\n')
+            new_row = True
+
+        if new_row:
+            ln_start_y = y
+            ln_end_y = y2
+
+            text.append(" " * cols)
+
+        if text[-1][start_col:start_col+len(word.value)].strip() != '':
+            print('\n---')
+            print(len(text), ln_start_y, ln_end_y, word, word.geometry)
+            print(text[-1])
+            print(text[-1][:start_col] + word.value + text[-1][start_col+len(word.value):])
+            print(" " * start_col + "^" + "-" * (len(word.value)-1))
+            print('---\n')
+            assert False
+
+        text[-1] = text[-1][:start_col] + word.value + text[-1][start_col+len(word.value):]
+
+    for row in text:
+        print(row)
+    return text
+
+
+def extract_receipt_items(text_rows: list[str]) -> list[dict]:
+    items = []
+
+    for line in text_rows:
+        stripped = line.strip()
+        match = re.search(r"(?P<item>.+?)\s{3,}(?P<price>(\$\s*)?(\d+,?)+\.?\d*)\s*(?P<tag>\w+)?", stripped)
+        if match:
+            items.append({
+                'item': match.group('item'),
+                'price': match.group('price'),
+                'tag': match.group('tag')
+            })
+
+    return items
+
+
+def convert_receipt_image_to_text(img_bytes: bytes, model: OCRPredictor, debug: bool) -> str:
+    doc = DocumentFile.from_images(img_bytes)
+    result = model(doc)
+
+    text_rows = convert_doc_page_to_text_grid(result.pages[0], debug)
+    return "\n".join(text_rows)
+
+
+def convert_receipt_image_to_df(img_bytes: bytes, model: OCRPredictor, debug: bool) -> pd.DataFrame:
+    doc = DocumentFile.from_images(img_bytes)
+    result = model(doc)
+
+    text_rows = convert_doc_page_to_text_grid(result.pages[0], debug)
+    items = extract_receipt_items(text_rows)
+    df = pd.DataFrame(items)
+    return df
